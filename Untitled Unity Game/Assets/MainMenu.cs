b3f6064244c649e85d7697afd23cda30f5f59f00
JenesesD <jenesesd@gmail.com>
2020-09-11 06:25:30 +0200

    Exit Game Button
    Game can now exit

--- Untitled Unity Game/Assets/MainMenu.cs
+++ Untitled Unity Game/Assets/MainMenu.cs
@@ -4,4 +4,16 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public void StartGame()
+    {
+
+    }
+
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game!");
+        Application.Quit();
+    }
 }
