eeb9548448cbc7362edd7ea78ed12352e40b0069
JenesesD <jenesesd@gmail.com>
2020-09-23 14:21:18 +0200

    Added player movement script

---
+++ Untitled Unity Game/Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 direction;
+
+    void Update()
+    {
+        TakeInput();
+        Move();
+    }
+
+    private void Move()
+    {
+
+    }
+
+
+    private void TakeInput()
+    {
+        direction = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+    }
+
+}
