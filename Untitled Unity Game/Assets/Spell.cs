7854f0b59494ae09a87c9a7209bb1509b5690634
JenesesD <jenesesd@gmail.com>
2020-09-23 23:02:33 +0200

    spell still doesn't deal damage
    created a script, for whatever reason doesn't work. Tomorrow awaits.

---
+++ Untitled Unity Game/Assets/Spell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour
+{
+    public GameObject projectile;
+    public float damage;
+    public float projectileVelocity;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject spell = Instantiate(projectile, transform.position, Quaternion.identity);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 playerPos = transform.position;
+            Vector2 direction = (mousePos - playerPos).normalized;
+            spell.GetComponent<Rigidbody2D>().velocity = direction * projectileVelocity;
+            spell.GetComponent<Projectile>().damage = damage;
+        }
+    }
+}
+
